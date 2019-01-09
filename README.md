ac7f36cc7a50856a260cc879175b5aa7f69714ee
angadsharma1016 <angadsharma1016@gmail.com>
2019-01-09 20:50:06 +0530

    REST

---
+++ README.md
@@ -0,0 +1,26 @@
+# REST API session
+
+<br />
+
+### Steps to run
+
+<br/>
+
+* Make sure to have postman and node.js installed
+
+* Create a new file app.js just as defined here
+
+* Open terminal/powershell where the file is
+
+* Run the following commands
+
+<br/>
+
+```
+npm i express body-parser
+node app
+```
+
+<br/>
+
+* Press <b> ctrl + c </b> to exit
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bp = require("body-parser")
+const app = express()
+
+app.use(bp.json())
+app.use(bp.urlencoded({extended:false}))
+
+app.get("/", (req,res)=>{
+    res.json({
+        message:"Hello there"
+    })
+})
+
+app.post("/", (req,res)=>{
+    res.json({message:"message received"})
+})
+
+app.listen("3000", ()=>console.log("listening"))
